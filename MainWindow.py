bce6cf77ab66096640fdfc9663347a5951a6670e
3ddy <edvard.jelusic@gmail.com>
2020-08-31 20:08:11 +0200

    Prebacio na svoj komp

--- MainWindow.py
+++ MainWindow.py
@@ -1,5 +1,8 @@
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from recepti import *
+from PyQt5.Qt import Qt
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -11,14 +14,17 @@ class Ui_MainWindow(object):
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
         self.textBrowser.setGeometry(QtCore.QRect(20, 90, 761, 481))
         self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser.setStyleSheet("background-color: #FFFDD7")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(270, 20, 113, 24))
         self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setStyleSheet("background-color: #FFFDD7")
         self.jelo = QtWidgets.QComboBox(self.centralwidget)
-        self.jelo.setGeometry(QtCore.QRect(60, 20, 81, 25))
+        self.jelo.setGeometry(QtCore.QRect(60, 20, 100, 25))
         self.jelo.setObjectName("comboBox")
+        self.jelo.setStyleSheet("background-color: #FFFDD7")
         self.meso = QtWidgets.QLabel(self.centralwidget)
-        self.meso.setGeometry(QtCore.QRect(190, 22, 111, 20))
+        self.meso.setGeometry(QtCore.QRect(180, 22, 111, 20))
         self.meso.setObjectName("label")
         self.naziv = QtWidgets.QLabel(self.centralwidget)
         self.naziv.setGeometry(QtCore.QRect(20, 22, 69, 20))
@@ -32,9 +38,11 @@ class Ui_MainWindow(object):
         self.pushButton_izracunaj = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_izracunaj.setGeometry(QtCore.QRect(470, 20, 89, 28))
         self.pushButton_izracunaj.setObjectName("pushButton")
+        self.pushButton_izracunaj.setStyleSheet("background-color: #12C300")
         self.pushButton_izlaz = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_izlaz.setGeometry(QtCore.QRect(20, 580, 89, 28))
         self.pushButton_izlaz.setObjectName("pushButton_2")
+        self.pushButton_izlaz.setStyleSheet("background-color: #FF7171")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 803, 22))
@@ -50,15 +58,21 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Preračun količina sastojaka u odnosu na količinu mesa"))
-        self.meso.setText(_translate("MainWindow", "Količina mesa:"))
+        self.meso.setText(_translate("MainWindow", "KOLIČINA MESA:"))
         self.grama.setText(_translate("MainWindow", "grama"))
-        self.Recept.setText(_translate("MainWindow", "Recept:"))
-        self.naziv.setText(_translate("MainWindow", "Jelo:"))
-        self.pushButton_izracunaj.setText(_translate("MainWindow", "Izracunaj"))
-        self.pushButton_izlaz.setText(_translate("MainWindow", "Zatvori"))
+        self.Recept.setText(_translate("MainWindow", "RECEPT:"))
+        self.naziv.setText(_translate("MainWindow", "JELO:"))
+        self.pushButton_izracunaj.setText(_translate("MainWindow", "IZRAČUNAJ"))
+        self.pushButton_izlaz.setText(_translate("MainWindow", "IZLAZ"))
         self.jelo.addItem("Tatarski")
         self.jelo.addItem("Bolognese")
         self.jelo.addItem("Hamburger")
+        self.jelo.addItem("Vojnicki Pasulj")
+
+    def KeyPressEvent(self, event):
+        if event.key() == QtCore.Key_Enter:
+            ispisi_recept()
+
 
 def izlaz(self):
     sys.exit()
@@ -74,18 +88,17 @@ def ispisi_recept(self):
             ui.textBrowser.setText(izracun_bolognese(kolicina_mesa))
         if ui.jelo.currentText() == "Hamburger":
             ui.textBrowser.setText(izracun_hamburger(kolicina_mesa))
+        if ui.jelo.currentText() == "Vojnicki Pasulj":
+            ui.textBrowser.setText((izracun_fazol(kolicina_mesa)))
     else:
         ui.textBrowser.setText("MOLIM UNESITE KOLIČINU MESA!")
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-
     ui.pushButton_izracunaj.clicked.connect(ispisi_recept)
-
     ui.pushButton_izlaz.clicked.connect(izlaz)
     MainWindow.show()
     sys.exit(app.exec_())
--- recepti.py
+++ recepti.py
@@ -1,7 +1,5 @@
 # TATARSKI BIFTEK
-
 def izracun_tatarski(kolicina_mesa):
-    kolicina_mesa = kolicina_mesa
     omjer = kolicina_mesa / 600
     tatarski_biftek = """Sastojci
 *************
@@ -13,44 +11,44 @@ def izracun_tatarski(kolicina_mesa):
 {smjesa}g kapara
 {smjesa}g svježeg peršinovog lista
 {smjesa}g luka
-      
+
 [majoneza]
 {zumanjci} žumanjka
 {senf} žličice Dijon senfa
 {limun} limun (sok)
 {ulje} ml suncokretovog ulja
 {kecap} žličice kečapa
-     
+
 [pomješati]
 [Začiniti]
 -Sol, Papar, Čili
-      
+
 [dodati u meso i miješati, dodavajući]
 {zacin} žličica Tabasco umaka
 {zacin} žličica Worcester umaka
 {zacin} žličica konjaka
-    
+
 Priprema
 ***********
 -Za pripremu tatarskog bifteka, najbolje je uzeti rep i prvu polovicu srednjeg dijela junećeg bifteka. Meso dobro očistite od svih masnoća i žilica pa ga usitnite.
 Možete ga strugati ili sjeckati nožem sve dok se ne postane mazivo.
-       
+
 – sitno nasjeckajte inćune, kisele krastavce, kapare, luk i peršinov list.
-        
+
 - zamiješajte majonezu od žumanjaka, senfa i soka jednog limuna, postupno dodajući ulje u tankoj niti ili kap po kap.
 U dobivenu majonezu dodajte kečap, dobro promiješajte pa dodajte ostale sastojke koje ste prethodno nasjeckali.
-       
+
 - Smjesu začinite solju, paprom i čilijem prema ukusu, a potom dodajte meso i miješajte tako da u jednu ruku uzmete žlicu, a u drugu vilicu.
-       
+
 - Smjesu na kraju začinite Tabasco i Worcester umakom te konjakom, dobro promiješajte i ostavite u hladnjaku da se sastojci sjedine.
-""".format(meso = kolicina_mesa, smjesa = round((omjer*10),1), zumanjci = round((omjer*2),1), \
-                   senf = round((omjer*2),1), limun = round((omjer),1), ulje = int(omjer*100), \
-                   kecap = round((omjer*2),1), zacin = round((omjer),1))
+""".format(meso=kolicina_mesa, smjesa=round((omjer * 10), 1), zumanjci=round((omjer * 2), 1), \
+           senf=round((omjer * 2), 1), limun=round((omjer), 1), ulje=int(omjer * 100), \
+           kecap=round((omjer * 2), 1), zacin=round((omjer), 1))
     return tatarski_biftek
+
 
 # BOLOGNESE
 def izracun_bolognese(kolicina_mesa):
-    kolicina_mesa = kolicina_mesa
     omjer = kolicina_mesa / 1000
     bolognese = """Sastojci:
 *********
@@ -79,18 +77,19 @@ Dolijte vino i kuhajte dok trećina ne ispari,
 posolite, popaprite. 
 Smanjite temperaturu i 
 umiješajte rajčicu i celer te dodajte lovorov list.
-Poklopite i pirjajte na laganoj vatri sat, sat i pol, uz povremeno miješanje.
+Poklopite i pirjajte na laganoj vatri oko 2 sata (original 4h) uz povremeno miješanje.
 
 Napomena:
 Ovo je originalni recept za umak bolognese koji je još 1982. potvrdila trgovačka komora Bologne. Želite li biti dosjedni originalu, umjesto spaghetta (špageta) treba koristiti tagliatelle.
-""".format(meso = kolicina_mesa, ulje = round((omjer*2),1), panceta = round((omjer*6),1), kapula = round((omjer*2),1), \
-           cesanj = round((omjer*3),1), mrkva = round((omjer*2),1), vino = round((omjer*2),1), pelati = int(omjer*800), \
-           lovor = round((omjer*2),1), pasta = int(omjer*900))
+""".format(meso=kolicina_mesa, ulje=round((omjer * 2), 1), panceta=round((omjer * 6), 1), kapula=round((omjer * 2), 1), \
+           cesanj=round((omjer * 3), 1), mrkva=round((omjer * 2), 1), vino=round((omjer * 2), 1),
+           pelati=int(omjer * 800), \
+           lovor=round((omjer * 2), 1), pasta=int(omjer * 900))
     return bolognese
 
-#HAMBURGER
+
+# HAMBURGER
 def izracun_hamburger(kolicina_mesa):
-    kolicina_mesa = kolicina_mesa
     omjer = kolicina_mesa / 350
     hamburger = """Sastojci:
 *********
@@ -112,6 +111,62 @@ sve dobro izmjesati i ostaviti bar jedan dan da odstoji, eventualno jos premjesi
 ispeci pljeske i prepeci peciva na toj masti 
 sloziti hamburgere po gustu..
 
-okus bude pomalo leskovacki.""".format(meso = kolicina_mesa, svinja = int(omjer*150), mast = round(omjer,1), \
-                                       bjelanjak = round((omjer*2),1), kapula = round(omjer,1), cesanj= round((omjer*2),1), konzerva = round(omjer,1))
+okus bude pomalo leskovacki.""".format(meso=kolicina_mesa, svinja=int(omjer * 150), mast=round(omjer, 1), \
+                                       bjelanjak=round((omjer * 2), 1), kapula=round(omjer, 1),
+                                       cesanj=round((omjer * 2), 1), konzerva=round(omjer, 1))
     return hamburger
+
+
+# FAZOL
+def izracun_fazol(kolicina_mesa):
+    omjer = kolicina_mesa / 100
+    fazol = """Sastojci:
+*********
+Fažol: 	{fazol} g
+Brašno tip800: 	{brasno} g
+Kapula:	{kapula} g
+Češanj:	{cesanj} g
+Mrkva:	{mrkva} g
+Konzerva 30%:	{konzerva} g
+Govedina:         {meso} g
+Mast:	{mast} g
+Paprika:	{paprika} g
+Papar:	{papar} g
+Peršin:	{persin} g
+Vegeta:	{vegeta} g
+Lovor:	{lovor} g
+Sol
+
+Priprema:
+*********
+Prebran pasulj potopiti u odgovarajuću količinu hladne vode i držati ga na hladnom mestu
+da se kvasi 1-3 ćasa. Zatim ocediti vodu u kojoj je pasulj potapan, pa pasulj staviti u pogodnu posudu,
+naliti hladnom vodom da ogrezne i kuvati 10-15 minuta od prokljućavanja.
+
+U međuvremenu na deo vrele masnoće dodati polovinu sitno seckanog crnog luka i mrkvu, sečenu na sitnije kockice
+ili rendisanu, malo propržiti, pa dodati meso prethodno odvojeno od kostiju i isećeno na kocke. 
+Sve zajedno dinstati dok ne ispari otpuštena tečnost, skinuti sa vatre | odložiti do upotrebe.
+
+Iz prokuvanog pasulja ocediti vodu pa dodati polovinu sitno seckanog crnog luka, prodinstana meso sa ostalim artiklima,
+po potrebi naliti toplom vodom ili fondom od kostiju da ogrezne i kuvati uz povremeno dolivanje tople vode
+dok svi artikli omekšaju.
+
+Preostalu masnoću zagrejati, dodati brašno i pržiti da blago porumeni. Kada brašno porumeni, isključiti toplotu
+pa odmahdodati papriku u prahu i tucani i sitno seckani beli luk i sve dobro Izmešati.
+
+Kada pasulj omekša dodati lovorov list, dodatak jelima, koncentrovani sok od paradajza, prethodno razmućen u toploj
+vodi, posoliti i zapržiti uz stalno mešanje pasulja da se zaprška ne zgrudva.
+
+Pasulj sa zaprškom kuvati istiha jo$ 20-30 minuta, posla ćega isključiti toplotu, dodati samleveni biber,
+sitno seckani peršunov list. probati ukus i po potrebi dosoliti.
+
+Napomena: ako nema uslova da ae meso, mrkva | luk crni prodinstaju, tada sa ovi artikh sitno iseckani 
+(meso isečeno na kocke) dodaju u pasul u vreme predviđeno recepturom za prodinstane ove artikle pa se sve skupa
+kuva dok svi artikli omekšaju i dalje produži tehnološki postupak predviđenom recepturom
+
+Pasulj se pri kirvanju ne sme mešati sve do zapržavanja da ne bi došlo do lomljenja zrna i zegorevanja.
+""".format(meso=kolicina_mesa, fazol=int(omjer * 120), brasno=round((omjer * 8), 1), kapula=round((omjer * 20), 1), \
+           cesanj=round((omjer), 1), mrkva=round((omjer * 10), 1), konzerva=round((omjer * 3), 1), \
+           mast=round((omjer * 20), 1), paprika=round((omjer * 0.3), 1), papar=round((omjer * 0.1), 1), \
+           persin=round((omjer), 1), vegeta=round((omjer), 1), lovor=round((omjer * 0.01), 1))
+    return fazol
